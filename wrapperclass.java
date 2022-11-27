c6f2666b4618274e4cb04f4e747a80ed5cff45f3
Smplegithubmokshita <100393432+Smplegithubmokshita@users.noreply.github.com>
2022-11-27 16:10:33 +0530

    wrapper class
    wrapper class

---
+++ wrapperclass.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ public class wrapperclass
+{
+  public static void main(String args[])
+  {
+    int a=20;
+    Integer i = IntegervalueOf(a);
+    Integer j=a;
+    System.out.println(a+" "+i+" "+j);
+    }
+    }
